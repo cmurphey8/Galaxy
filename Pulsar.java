4255826da339c6c291ba8f94eb4ca01bf40c9c24
cmurphey8 <53098445+cmurphey8@users.noreply.github.com>
2021-05-24 20:35:10 -0400

    Update Pulsar.java

--- Pulsar.java
+++ Pulsar.java
@@ -3,26 +3,30 @@
  *  GOAL: Complete step(...) method to add pulse functionality
  *              
  **********************************************************************************/
-
 public class Pulsar extends Body {
 
-    private boolean imageRev;
-    private int imageCount;
+    private boolean imageRev;   // is the image reversed?
+    private int imageCount;     // number of steps taken since last image reversal
+
+    // images to swap between every 10 steps
+    String image = "pulsar.gif";
+    String reverseImage = "pulsarR.gif";
 
     // create and init a new object with input parameters scanned from a .txt file
     public Pulsar(double[] arr, double R) {
         super(arr, R);
         newImage("pulsar.gif");
-
+        
         imageCount = 0;
         imageRev = false;
     }
 
-    // use Body step update and then attempt to update image
+    // attempt to update image after calling Body class step method
     @Override
     public void step(double dt, double R) {
+        // call the Body class step method
         super.step(dt, R);
-        // TODO: swap the image between "pulsar.gif" and "pulsarR.gif" every 10 steps
-        //       (see imageRev, imageCount)
+
+        // TODO: update this Pulsar object's image from image to reverseImage every 10 steps
     }
 }
